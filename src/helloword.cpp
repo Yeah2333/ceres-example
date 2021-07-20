f1f35ae8cabd2de005567226ea9ac266c9f99d77
Yeah2333 <653091941@qq.com>
2021-07-20 18:59:13 +0800

    finish helloword's numeric diff and analytic diff

--- src/helloword.cpp
+++ src/helloword.cpp
@@ -9,6 +9,44 @@ struct CostFunctor {
         return true;
     }
 };
+struct NumericDiffCostFunctor {
+    bool operator()(const double* const x, double* residual) const{
+        residual[0] = 10.0 - x[0];
+        return true;
+    }
+};
+// A CostFunction implementing analytically derivatives for the
+// function f(x) = 10 - x.
+class QuardraticCostFunction:public ceres::SizedCostFunction<1 /* number of residuals */,
+        1 /* size of first parameter */ >{
+public:
+    virtual ~QuardraticCostFunction(){}
+
+    virtual bool Evaluate(double const* const* parameters,
+                          double* residuals,
+                          double** jacobians) const{
+        double x = parameters[0][0];
+        // f(x) = 10 - x.
+        residuals[0] = 10.0 - x;
+
+        // f'(x) = -1. Since there's only 1 parameter and that parameter
+        // has 1 dimension, there is only 1 element to fill in the
+        // jacobians.
+        //
+        // Since the Evaluate function can be called with the jacobians
+        // pointer equal to NULL, the Evaluate function must check to see
+        // if jacobians need to be computed.
+        //
+        // For this simple problem it is overkill to check if jacobians[0]
+        // is NULL, but in general when writing more complex
+        // CostFunctions, it is possible that Ceres may only demand the
+        // derivatives w.r.t. a subset of the parameter blocks.
+        if (jacobians!= nullptr && jacobians[0] != nullptr) {
+            jacobians[0][0] = -1;
+        }
+        return true;
+    }
+};
 int main(int argc, char** argv) {
     google::InitGoogleLogging(argv[0]);
 
@@ -20,10 +58,18 @@ int main(int argc, char** argv) {
     ceres::Problem problem;
 
     //Set up the only cost function(also known as residual). This uses
-    //auto-differentiation to obtain the derivative(jacobian).
-    ceres::CostFunction* cost_function =
-            new ceres::AutoDiffCostFunction<CostFunctor,1,1>(new CostFunctor);
-    problem.AddResidualBlock(cost_function, nullptr, &x);
+    ///auto-differentiation to obtain the derivative(jacobian).
+    //ceres::CostFunction* cost_function =
+    //        new ceres::AutoDiffCostFunction<CostFunctor,1,1>(new CostFunctor);
+    //problem.AddResidualBlock(cost_function, nullptr, &x);
+    ///NumericDifferentitation
+//    ceres::CostFunction* costFunction = new ceres::NumericDiffCostFunction<NumericDiffCostFunctor, ceres::CENTRAL, 1, 1>(
+//            new NumericDiffCostFunctor
+//            );
+//    problem.AddResidualBlock(costFunction, nullptr, &x);
+    /// Analytic function
+    ceres::CostFunction* costFunction = new QuardraticCostFunction;
+    problem.AddResidualBlock(costFunction,NULL, &x);
 
     //Run the solver!
     ceres::Solver::Options options;
